ffa61e2b3acbb11830944a305facf50b6ee3718f
Gaston Ciancio <gastonciancio4@gmail.com>
2022-11-11 00:15:17 -0300

    proveedores con busqueda, renegociamineto y reserva, falta pulir detalles

--- app/helpers/providers_maker_api.py
+++ app/helpers/providers_maker_api.py
@@ -19,40 +19,28 @@ def providers_makers_auth():
     
     return res
 
-def get_providers_by_data(params):
-
+def get_providers_with_only_materials():
+    
     providers_makers_auth()
-
-    filtro_precio = params['filtro_precio']
-    dias_extra = params['dias_extra']
-    #materiales = params['materiales']
-
-   
-    #materiales_to_api = []
-    #for material in materiales:
-    #    materiales_to_api.append({"name": material["name"],"amount": material["amount"],"date_required": material["date_required"]})
 
     reqSession = requests.Session()
 
     api_url = 'https://apidssd.fly.dev/suppliers/by_data'
+
+    #CAMBIAR POR MATERIALES QUE POSTA SE NECESITAN
     body = { "materiales":[  
                     {
                         "name": "Madera",
-                        "amount": 15,
+                        "amount": 1,
                         "date_required": "17/11/2022"
                     },
                     {
-                        "name": "Terracota",
-                        "amount": 15,
-                        "date_required": "17/11/2022"
+                        "name": "Vidrio",
+                        "amount": 1,
+                        "date_required": "18/11/2022"
                     }
                 ]
             }
-
-    if (params['filtro_precio'] != '' or params['filtro_precio'] != None):
-        body['filtro_precio'] = int(filtro_precio)
-    if (params['dias_extra'] != '' or params['dias_extra'] != None):
-        body['dias_extra'] = int(dias_extra)
 
     headers =  {'Authorization': 'Bearer '+session['token_providers']}
 
@@ -64,45 +52,58 @@ def get_providers_by_data(params):
         proveedores = res.json()
     return proveedores
 
-def reserve_providers_by_data(data):
+
+def get_providers_by_data(params):
+
+    providers_makers_auth()
+
+    reqSession = requests.Session()
+
+    api_url = 'https://apidssd.fly.dev/suppliers/by_data'
+
+    #CAMBIAR POR MATERIALES QUE POSTA SE NECESITAN
+    body = { "materiales":[  
+                    {
+                        "name": "Madera",
+                        "amount": 1,
+                        "date_required": "17/11/2022"
+                    },
+                    {
+                        "name": "Vidrio",
+                        "amount": 1,
+                        "date_required": "18/11/2022"
+                    }
+                ]
+            }
+    if (params['filtro_precio'] != '' and params['filtro_precio'] != None):
+        body['filtro_precio'] = int(params['filtro_precio'])
+    if (params['dias_extra'] != '' and params['dias_extra'] != None):
+        body['dias_extra'] = int(params['dias_extra'])
+
+    headers =  {'Authorization': 'Bearer '+session['token_providers']}
+
+    res = reqSession.post(api_url, json=body, headers=headers)
+    proveedores = None
+    if(res.status_code < 200 or res.status_code > 299):
+        print("Fallo obtener proveedores by data", flush=True)
+    else:
+        proveedores = res.json()
+    return proveedores
+
+def reserve_providers_by_data(body):
     providers_makers_auth()
 
     reqSession = requests.Session()
 
     api_url = 'https://apidssd.fly.dev/suppliers/reserve'
-    body = {
-            "suppliers": [
-                {
-                "id": 1,
-                "materials": [
-                    {
-                    "id": 2,
-                    "amount": 1
-                    }
-                ]
-                },
-                {
-                "id": 2,
-                "materials": [
-                    {
-                    "id": 1,
-                    "amount": 1
-                    }
-                ]
-                }
-            ]
-            }
 
     headers =  {'Authorization': 'Bearer '+session['token_providers']}
 
-    breakpoint()
-
-    res = reqSession.post(api_url, data=json.dumps(body), headers=headers)
+    res = reqSession.post(api_url, json=body, headers=headers)
     response = None
     if(res.status_code < 200 or res.status_code > 299):
         print("Fallo reserva de proveedores by data", flush=True)
     else:
         response = res.json()
-    breakpoint()
     return response
     
--- app/resources/provider.py
+++ app/resources/provider.py
@@ -1,19 +1,114 @@
-from flask import render_template, request
+from flask import render_template, request,redirect,url_for, session
 from flask.helpers import flash
+import re
 
-from app.helpers.providers_maker_api import get_providers_by_data, reserve_providers_by_data
+from app.helpers.providers_maker_api import get_providers_by_data, reserve_providers_by_data, get_providers_with_only_materials
 
 def index():
-    return render_template('providers/reserve_providers.html')
+    response = get_providers_with_only_materials()
+    print(response)
+    materiales_sin_prov = response['metadata']['materiales_sin_proveedor']
+    materiales_con_prov = response['suppliers']
+    session['materiales_con_prov'] = materiales_con_prov
+    session['materiales_sin_prov'] = response['metadata']['materiales_sin_proveedor']
+
+    #CAMBIAR CUANDO TENGAMOS LOS MATERIALES Y SUS CANT ESTABLECIDOS
+    materiales_pedidos = [  
+                    {
+                        "name": "Madera",
+                        "amount": 1,
+                        "date_required": "17/11/2022"
+                    },
+                    {
+                        "name": "Vidrio",
+                        "amount": 1,
+                        "date_required": "18/11/2022"
+                    }
+                ]
+
+    for material in materiales_pedidos:
+        for supplier_with_materials in materiales_con_prov:
+            for material_prov in  supplier_with_materials['materials']:
+                if material_prov['name'].lower() == material['name'].lower():
+                    material_prov['amount'] = material['amount']
+    
+
+    return render_template('providers/reserve_providers.html',materiales_con_prov=materiales_con_prov, materiales_sin_prov=materiales_sin_prov)
 
 def search():
     response = get_providers_by_data(request.form)
+    print(response)
     materiales_sin_prov = response['metadata']['materiales_sin_proveedor']
     materiales_con_prov = response['suppliers']
+    session['materiales_con_prov'] = materiales_con_prov
+    session['materiales_sin_prov'] = response['metadata']['materiales_sin_proveedor']
+
+    #CAMBIAR CUANDO TENGAMOS LOS MATERIALES Y SUS CANTIDADES ESTABLECIDOS
+    materiales_pedidos = [  
+            {
+                "name": "Madera",
+                "amount": 1,
+                "date_required": "17/11/2022"
+            },
+            {
+                "name": "Vidrio",
+                "amount": 1,
+                "date_required": "18/11/2022"
+            }
+        ]
+
+    for material in materiales_pedidos:
+        for supplier_with_materials in materiales_con_prov:
+            for material_prov in  supplier_with_materials['materials']:
+                if material_prov['name'].lower() == material['name'].lower():
+                    material_prov['amount'] = material['amount']
+
     return render_template('providers/reserve_providers.html',materiales_con_prov=materiales_con_prov, materiales_sin_prov=materiales_sin_prov)
 
 
 def reserve():
-    response = reserve_providers_by_data(request.form)
-    flash(response)
-    return render_template('providers/reserve_providers.html')
+    provedores_elegidos = request.form.getlist("seleccionar_proveedor")
+    valores = []
+    for proveedor in provedores_elegidos:
+        prov = re.findall(r'\d+',proveedor)
+        prov = [int(num) for num in prov]
+        valores.append(prov)
+    valores = sorted(valores)
+    suppliers = []
+
+    if (len(valores) == 0):
+        flash('Debe elegir proveedores antes de reservar')
+        return redirect(url_for("providers_form")) 
+
+    i=0
+    valor_antiguo= valores[i][0]
+    while (i<len(valores)):
+        materiales = []
+        while (i<len(valores) and valores[i][0] == valor_antiguo):
+            materiales.append(
+                {
+                    "id": valores[i][1],
+                    "amount": valores[i][2]
+                }
+            )
+            valor_antiguo = valores[i][0]
+            i = i + 1
+        suppliers.append(
+            { 
+                "id": valor_antiguo,
+                "materials": materiales
+            }
+        )
+        if (i<len(valores)):
+            valor_antiguo = valores[i][0]
+        
+
+
+    body = {"suppliers": suppliers}
+    response = reserve_providers_by_data(body)
+    if (response !=None):
+        for message in response['response']:
+            flash(message)
+    else:
+        flash('Fallo la reserva')
+    return redirect(url_for("providers_form"))
